1baeaae6f4232c54f242029d2c8244a4374dd301
Ty Flannagan <tylerflannagan@yahoo.com>
2021-02-13 14:37:11 -0800

    update- headline fix

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Fast Feedback
+          Fast Feedback//Faster
         </h1>
 
         <p className={styles.description}>
